b0ba5a92aeeae67bf4f3bc2e1f36ceef4f67de7b
RJannat <ramimk097@gmail.com>
2025-05-05 14:53:44 +0600

    2nd pattern has printed

--- week-4/Module-13/pyramidPattern.c
+++ week-4/Module-13/pyramidPattern.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    int n;
+    int n, star = 1;
     scanf("%d", &n);
-    int star = n;
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= star; j++)
         {
             printf("* ");
         }
-        star--;
+        star++;
         printf("\n");
     }
     return 0;
