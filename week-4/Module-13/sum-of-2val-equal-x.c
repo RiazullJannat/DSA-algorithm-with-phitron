8d27aec389f7e9353e9969142be68bac45fe7b1f
RJannat <ramimk097@gmail.com>
2025-05-05 20:03:14 +0600

    fixed a bug

--- week-4/Module-13/sum-of-2val-equal-x.c
+++ week-4/Module-13/sum-of-2val-equal-x.c
@@ -11,7 +11,7 @@ int main()
     scanf("%d", &x);
     for (int i = 0; i < n; i++)
     {
-        for (int j = i + 1; i < n; i++)
+        for (int j = i + 1; j < n; j++)
         {
             if (arr[i] + arr[j] == x)
             {
