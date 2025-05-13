f5e71d73c5454b3439677a465ee128d2d69a1f1c
RJannat <ramimk097@gmail.com>
2025-05-13 11:56:46 +0600

    cheaking primary diagonal matrix

---
+++ week-4/Module-18/cheaking-diagonal-matrix.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int main()
+{
+    int row, col;
+    scanf("%d %d", &row, &col);
+    int array[row][col];
+    // taking input;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            scanf("%d", &array[i][j]);
+        }
+    }
+    int flag = 1;
+    if (row == col)
+    {
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                if (i != j && array[i][j] != 0)
+                {
+                    flag = 0;
+                }
+            }
+        }
+    }
+    else
+    {
+        printf("not primary diagonal matrix.");
+    }
+    if (flag == 1)
+    {
+        printf("This is primary diagonal matrix.\n");
+    }
+    else
+    {
+        printf("not primary diagonal matrix.");
+    }
+    
+
+    return 0;
+}
